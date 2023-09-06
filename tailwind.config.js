c602923903032c67c9bae4df983d4d78893579a2
Yann Kamche <yannkamche@gmail.com>
2023-09-06 16:26:35 +0100

    Added extended features

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,19 @@ module.exports = {
   darkMode: 'class',
   content: ["./*.{html,js}"],
   theme: {
+    container: { 
+      center: true,
+      padding: '1rem'
+    },
     extend: {
       fontFamily: {
         poppins: ['Poppins'],
         roboto: ['Roboto']
+      }, 
+      colors: {
+        "primary": '#FD3D57',
+        "bg-color": '#F8F8FF',
+        "secondary": '#001f3f'
       }
     },
   },
